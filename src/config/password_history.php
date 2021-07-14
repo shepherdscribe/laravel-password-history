4b662d6afef4e5e646b5c87345438807280397cf
Shepherd Scribe <62448059+shepherdscribe@users.noreply.github.com>
2021-07-15 00:22:50 +0800

    Update password_history.php

--- src/config/password_history.php
+++ src/config/password_history.php
@@ -16,7 +16,7 @@ return [
      * The models to be observed on the "saved" event
      */
     'models' => [
-        'App\Modes\User' => [
+        'App\Models\User' => [
             'password_column' => 'password',
             'guard' => 'user',
         ],
